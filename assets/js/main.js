2ebc1bfd5e64812eaf8a2dd64588681937a384b3
Lynn <lynn9388@gmail.com>
2019-04-11 14:34:04 +0800

    Add return to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,16 @@
-function toggleMenu() {
-  var nav = document.getElementsByClassName("site-header-nav")[0];
-  if (nav.style.display == "inline-flex") {
-    nav.style.display = "none";
-  } else {
-    nav.style.display = "inline-flex";
-  }
-}
-
-jQuery(function() {
-  // 回到顶部
-  function toTop () {
-    var $toTop = $(".gotop");
-
-    $(window).on("scroll", function () {
-      if ($(window).scrollTop() >= $(window).height()) {
-        $toTop.css("display", "block").fadeIn();
-      } else {
-        $toTop.fadeOut();
-      }
-    });
-
-    $toTop.on("click", function (evt) {
-      var $obj = $("body,html");
-      $obj.animate({
-        scrollTop: 0
-      }, 240);
-
-      evt.preventDefault();
-    });
-  }
-
-  toTop();
+// Return to top
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.return-to-top').fadeIn(200);
+    } else {
+        $('.return-to-top').fadeOut(200);
+    }
 });
+
+$(function () {
+    $(".return-to-top").click(function () {
+        $("html,body").animate({
+            scrollTop: $("head").offset().top
+        }, "500");
+    })
+})
